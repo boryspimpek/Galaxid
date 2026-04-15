373f8661e358b11eb0b96f04b91a35001820de0f
boryspimpek <borysgame@gmail.com>
2026-04-15 13:45:25 +0200

    warnings

--- scenes/player/DamageSystem.gd
+++ scenes/player/DamageSystem.gd
@@ -10,9 +10,4 @@ var player: CharacterBody2D
 func _ready():
 	player = get_parent()
 
-# TODO: Podłącz sygnały kolizji w Player.tscn (Area2D)
 
-func take_damage(amount: int):
-	# TODO: Zmniejsz armor gracza
-	# TODO: Obsłuż śmierć
-	pass
