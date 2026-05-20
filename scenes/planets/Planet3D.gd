75b1db229ea76941d05483819000bb4a436ea152
boryspimpek <borysgame@gmail.com>
2026-05-20 16:09:18 +0200

    formations

--- scenes/planets/Planet3D.gd
+++ scenes/planets/Planet3D.gd
@@ -1,19 +1,8 @@
 extends Node2D
 
 @export var move_speed: float = 30.0
-@export var rotation_speed: float = 0.1
-
-@onready var _viewport: SubViewport = $SubViewportContainer/SubViewport
 
 func _process(delta: float) -> void:
 	position.y += move_speed * delta
 	if position.y > 2300.0:
 		queue_free()
-	_rotate_planet(delta)
-
-func _rotate_planet(delta: float) -> void:
-	if rotation_speed == 0.0:
-		return
-	for child in _viewport.get_children():
-		if child is Node3D:
-			child.rotate_y(rotation_speed * delta)
