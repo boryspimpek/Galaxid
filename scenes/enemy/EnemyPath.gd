10c58e81b114b1ab0e0fad57f919d77cefc2a25a
boryspimpek <borysgame@gmail.com>
2026-05-20 17:09:17 +0200

    formations

--- scenes/enemy/EnemyPath.gd
+++ scenes/enemy/EnemyPath.gd
@@ -3,11 +3,20 @@ extends PathFollow2D
 @export var speed: float = 60.0
 @export var remove_at_end: bool = true
 
+var _activated := false
+
 func _ready():
 	set_process(false)
 
 func activate():
+	if _activated:
+		return
+	_activated = true
 	set_process(true)
+	if get_parent():
+		for sibling in get_parent().get_children():
+			if sibling.has_method("activate"):
+				sibling.activate()
 
 func _process(delta: float):
 	progress += speed * delta
